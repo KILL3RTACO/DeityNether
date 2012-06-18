0e3d67ca1c8faec4e35ddd1f66bcd84be83a9131
Taco <kill3rtaco@gmail.com>
2012-06-18 09:12:04 -0700

    added to config

--- src/com/imdeity/deitynether/Config.java
+++ src/com/imdeity/deitynether/Config.java
@@ -3,6 +3,9 @@ package com.imdeity.deitynether;
 import java.io.File;
 import java.io.IOException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Config{
@@ -16,14 +19,26 @@ public class Config{
 	}
 	
 	public void loadDefaults(){
-		if(!config.contains("nether.days-until-next-regen"))
-			config.set("nether.days-until-next-regen", 7);
-		if(!config.contains("nether.main-world"))
-			config.set("nether.main-world", "world");
-		if(!config.contains("nether-world"))
-			config.set("nether.nether-world", "world_nether");
-		if(!config.contains("nether.pigman-gold-drop-chance"))
-			config.set("nether.pigman-gold-drop-chance", 10);
+		if(!config.contains("world.nether.days-until-next-regen"))
+			config.set("world.nether.days-until-next-regen", 7);
+		if(!config.contains("world.main.name"))
+			config.set("world.main.name", "world");
+		if(!config.contains("world.nether.name"))
+			config.set("world.nether.name", "world_nether");
+		if(!config.contains("world.nether.pigman-gold-drop-chance"))
+			config.set("world.nether.pigman-gold-drop-chance", 10);
+		if(!config.contains("spawn.main-world.x"))
+			config.set("spawn.main.x", 100);
+		if(!config.contains("spawn.main.y"))
+			config.set("spawn.main.y", 64);
+		if(!config.contains("spawn.main.z"))
+			config.set("spawn.main.z", 100);
+		if(!config.contains("spawn.nether.x"))
+			config.set("spawn.nether.x", 100);
+		if(!config.contains("spawn.nether.y"))
+			config.set("spawn.nether.y", 64);
+		if(!config.contains("spawn.nether.z"))
+			config.set("spawn.nether.z", 100);
 		save();
 	}
 	
@@ -36,19 +51,35 @@ public class Config{
 	}
 	
 	public int getDaysUntilNextRegen(){
-		return config.getInt("nether.days-until-next-regen");
+		return config.getInt("world.nether.days-until-next-regen");
 	}
 	
 	public int getDropChance(){
-		return config.getInt("nether.pigman-gold-drop-chance");
+		return config.getInt("world.nether.pigman-gold-drop-chance");
 	}
 	
 	public String getMainWorldName(){
-		return config.getString("nether.main-world");
+		return config.getString("world.main.name");
+	}
+	
+	public Location getMainWorldSpawn(){
+		int x = config.getInt("spawn.main.x");
+		int y = config.getInt("spawn.main.y");
+		int z = config.getInt("spawn.main.z");
+		World world = Bukkit.getServer().getWorld(getMainWorldName());
+		return new Location(world, x, y, z);
+	}
+	
+	public Location getNetherWorldSpawn(){
+		int x = config.getInt("spawn.nether.x");
+		int y = config.getInt("spawn.nether.y");
+		int z = config.getInt("spawn.nether.z");
+		World world = Bukkit.getServer().getWorld(getNetherWorldName());
+		return new Location(world, x, y, z);
 	}
 	
 	public String getNetherWorldName(){
-		return config.getString("nether.nether-world");
+		return config.getString("world.nether.name");
 	}
 	
 }
